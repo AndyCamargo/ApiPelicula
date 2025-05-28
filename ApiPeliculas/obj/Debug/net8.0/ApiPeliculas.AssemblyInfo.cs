86015ccec59d0e4dce7384b35840522e90e8e0f6
AndyCamargo <andycamar001@gmail.com>
2025-05-28 11:55:26 -0500

    se creo temporal
    se creo el archivo para una prueba

--- ApiPeliculas/obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
+++ ApiPeliculas/obj/Debug/net8.0/ApiPeliculas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44de765af0f4cc43f8fe2cac3595d2e1afa3b33e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfbea4be95c39a6821fdba044c612cadd3d36d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiPeliculas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
